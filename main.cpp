9e318b4a841dc5e91dcc42460c65fb477bd1e0c9
Sebastian Dixon <smedixon@gmail.com>
2020-04-22 12:28:12 +0100

    enum, structure, union

--- main.cpp
+++ main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    int array[10];
-    int array2[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int array3[10] = {1, 2, 3, 4};
+    struct coffee
+    {
+        string name;
+        int strength;
+        double price;
+    };
+    coffee cocoa = {"Cocoa truffle", 10, 0.47};
 
-    int num = array3[3];
+    coffee vanilla;
+    vanilla.name = "vanilla eclair";
+    vanilla.strength = 9;
+    vanilla.price = 0.47;
 
-    char String[7] = {'s', 't', 'r', 'i', 'n', 'g', '\0'};
-    char notString[6] = {'s', 't', 'r', 'i', 'n', 'g'};
+    union aUnion
+    {
+        int intValue;
+        long longValue;
+        double doubleValue;
+    };
+    aUnion myUnion;
 
-    char aString[] = "string";
-    char anotherOne[] = "notastring";
+    myUnion.intValue = 3;
+    //cout << myUnion.intValue << endl;
+    myUnion.doubleValue = 4.5;
+    //cout << myUnion.doubleValue << endl;
+    //cout << myUnion.intValue; cout << endl;
 
-    std::cout <<aString;
-    std::cout <<String;
+    enum day {sunday, monday, tuesday, wednesday, thursday, friday, saturday};
 
+    day noWork {saturday};
 
+    cout<< noWork << endl;
     return 0;
 }
