71c925f40820e1f34c9c93f93e5a3ead122bc3f4
Sebastian Dixon <smedixon@gmail.com>
2020-04-23 12:31:43 +0100

    ternary, if, else if, else, switch, while, for, break

--- main.cpp
+++ main.cpp
@@ -5,37 +5,53 @@ using namespace std;
 
 int main()
 {
-    struct coffee
+    char response{'y'};
+    if (response == 'y' || response == 'Y')
     {
-        string name;
-        int strength;
-        double price;
-    };
-    coffee cocoa = {"Cocoa truffle", 10, 0.47};
+        cout << "correct response" << endl;
+    }
 
-    coffee vanilla;
-    vanilla.name = "vanilla eclair";
-    vanilla.strength = 9;
-    vanilla.price = 0.47;
-
-    union aUnion
+    else if (response == 'n' || response == 'N')
     {
-        int intValue;
-        long longValue;
-        double doubleValue;
-    };
-    aUnion myUnion;
+        cout << "incorrect response" << endl;
+    }
 
-    myUnion.intValue = 3;
-    //cout << myUnion.intValue << endl;
-    myUnion.doubleValue = 4.5;
-    //cout << myUnion.doubleValue << endl;
-    //cout << myUnion.intValue; cout << endl;
+    else
+    {
+        cout << "invalid input" << endl;
+    }
 
-    enum day {sunday, monday, tuesday, wednesday, thursday, friday, saturday};
+    int value = 1;
+    switch (value)
+    {
+        case '1':
+            cout<< "this is a one" << endl;
+            break;
+        case '2':
+            cout<< "this is a two not a one, wrong" << endl;
+            break;
+        case '4':
+            cout<< "this is 4, guess again, wrong" << endl;
+            break;
+        default:
+            cout<< "this is not a one, two or four"<< endl;
+            break;
+    }
 
-    day noWork {saturday};
+    int i = 2, j = 3;
+    cout << (i > j ? i : j) << " is greater" << endl;
 
-    cout<< noWork << endl;
+    for(int x = 0; x<5; ++x)
+    {
+        cout << "x is this value:" + x << endl;
+    }
+
+    int count = 0;
+    while (count<10)
+    {
+        count +=1;
+        cout << "the value of count is:" + count << endl;
+    }
+
     return 0;
 }
