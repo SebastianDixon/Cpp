80153e3159dac0f06ab457a7ac35c43a6139cce0
Sebastian Dixon <smedixon@gmail.com>
2020-04-27 13:38:24 +0100

    classes, encapsulation, publi, private

--- main.cpp
+++ main.cpp
@@ -3,22 +3,65 @@
 
 using namespace std;
 
-int sum(int x, int y) //first sum function
-{
-    int val;
-    val = x + y;
-    cout << val << endl;
-    return x + y;
-}
 
-int sum(int x, int y, int z) //second sum function
+
+class Player
 {
-    return x + y + z;
-}
+public:
+    string name;
+    int health{};
+    int damage{};
+
+    void print()
+    {
+        cout << name << " " << health<< " " << damage<< " " << '\n';
+    }
+
+    void calc(int i, int j)
+    {
+        auto result {i + j};
+
+        cout << result << endl;
+    }
+};  
+
+
+class Rectangle
+{
+    private:
+        int width{};
+        int height{};
+    
+    public:
+
+        void get_width() {cout<< width;}
+
+        void get_height() {cout<< height;}
+
+};
+
+class Base{
+    int x;
+
+    public:
+        void getA(int a) {a = x;}
+        int getX() {return x;}
+
+};
 
 
 int main()
 {
-    return sum(3, 4);
+    Player p3 {"Seb", 50, 100};
+
+    p3.print();
+    p3.calc(3,4);
+
+    Rectangle small;
+    small.get_height();
+    
+    
+    return 0;
+
 }
             
