d6cf52518b64f4a1bb5816f4eaf53078c85fc42b
Sebastian Dixon <smedixon@gmail.com>
2020-04-29 15:07:59 +0100

    reference value as function arguments

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void passByRed(int &);
+void passByRef(int &);  //function prototype shown to be given a reference argument in its parameters
 
 int main()
 {
     int num = 3;
-    int * pNum;     //pointer declared using * operator
+    cout << "num starts as: " << num << endl;
 
-    pNum = &num;    //the pointer pNum is assigned to the address for num
-    *pNum = 45;     //the value stored at the address under pNum is assigned the value 45, dereference operator
+    passByRef(num);
 
-    int num2 = 3;
-    int & refNum = num2;    //reference declred with & operator
-    refNum = 4;
+    cout << "num value referenced after function increment: " << num << endl;
+    
+    return num;
+}
 
-    cout << num2 << endl;
-    cout << refNum << endl;
-    cout << &num2 << '\n' << &refNum << endl;
+void passByRef(int & refNum)
+{
+    cout << "reference value passed as: " << refNum << endl;
 
+    ++refNum;
 
-    return 0;
+    cout << "reference value incremented: " << refNum << endl;
 }
