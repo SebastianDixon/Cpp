bb0b8cef1c36e227a5ad94e3f04633bdd3435802
Sebastian Dixon <smedixon@gmail.com>
2020-05-02 11:47:48 +0100

    pointers lab

--- main.cpp
+++ main.cpp
@@ -1,74 +1,96 @@
 #include <iostream>
 using namespace std;
 
+void PassByValue(int val)
+{
+    cout << "I am in PassByValue" << endl;
+
+    ++val;
+
+    cout << val << " is the value in the function" << endl;
+}
+
+void PassByRef(int & val)
+{
+    cout << "I am in PassByRef" << endl;
+
+    val = 50;
+
+    cout << val << " is the value in the reference function" << endl;
+}
 
 class Person
 {
-    private:
-    string middle_name;
-
     public:
-    string first_name;
-    string last_name;
-
-    int age;
-
-    string get_mid()
-    {
-        return middle_name;
-    }
-
-    void get_age()
-    {
-        cout << age << endl;
-    }
+        string name;
+        int age;
+        double height;
+        double weight;
 
     Person()
     {
-        cout << "Person\n";
+        cout << "Person" << endl;
     }
 
-    Person(string first_name, string last_name)
+    Person(string name, int age, double height, double weight)
     {
-        this->first_name = first_name;
-        this->last_name = last_name;
-    }
-
-    Person(string first_name, string last_name, int age)
-    {
-        this->first_name = first_name;
-        this->last_name = last_name;
+        this->name = name;
         this->age = age;
+        this->height = height;
+        this->weight = weight;
     }
-
-    Person(string first_name, string last_name, int age, string middle_name)
-    {
-        this->first_name = first_name;
-        this->last_name = last_name;
-        this->age = age;
-        this->middle_name = middle_name;
-    }
-
-    ~Person()
-    {
-        cout << "Destroyed Function" << endl;
-    }
-
+    
 };
+
+void ModifyPerson(Person & ob1)
+{   
+    cout << "inside modifyperson function" << endl;;
+
+    ob1.name = "Bob";
+}
+
+
 
 int main()
 {
 
-    // Person * p1 = new Person("Billy", "Bob", 70, "PHILLANTHROPACOUS");
-    Person * p1 = new Person;
+    Person p1{"Fred", 20, 185.2, 75};
+    cout << p1.name << '\n' << p1.age << '\n' << p1.height << '\n' << p1.weight << endl;
 
-    cout << &p1 << endl;
-    
-    *p1 = Person("Billy", "Bob", 70, "PHILLANTHROPACOUS");
+    ModifyPerson(p1);
 
-    cout << p1 << endl;
+    cout << p1.name << " is the name after passing to the function" << endl;
 
-    delete p1;
-    
+
+    int num1{};
+    int num2{};
+    int & pNum = num2;
+
+    num1 = 3;
+    pNum = 5;
+
+    PassByValue(num1);
+
+    cout << num1 << " is the value in the main class" << endl;
+
+    PassByRef(pNum);
+
+    cout << num2 << " is the value in the main class from PassByRef" << endl;
+
+    PassByValue(pNum);
+
+    cout << num2 << " is the value in the main class from PassByValue" << endl;
+
+    double * pDouble = new double;
+
+    *pDouble = 5.0;
+
+    cout << *pDouble << " This is the value after dereferencing" << endl;
+
+    delete pDouble;
+
+    cout << *pDouble << " This is the value after deleting" << endl;
+
+
     return 0;
 }
