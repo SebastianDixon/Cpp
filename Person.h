c6c6a536ef7e44518a24d42ac95f6dd275195f27
Sebastian Dixon <smedixon@gmail.com>
2020-05-05 13:28:00 +0100

    namespaces

--- Person.h
+++ Person.h
@@ -8,28 +8,22 @@ class Person
     private:
     string first_name;
     string last_name;
-
     int age;
 
     public:
-
     Person();
 
-
     Person(string first_name, string last_name);
-
 
     Person(string first_name, string last_name, int age);
 
     ~Person();
+
 
     void get_iD();
 
     void sayHello();
 
     void setfName(string fName);
-
-
-
 
 };
--- main.cpp
+++ main.cpp
@@ -3,10 +3,27 @@
 
 using namespace std;
 
+namespace Microsoft
+    {
+        namespace Geometry
+        {
+            const double PI = 3.14159;
+    
+            double Area(double radius)
+            {
+                return PI*(radius*radius);
+            }
+        }
+        
+    }
+
+using namespace Microsoft;
+
 
 int main()
 {
-
+    double val = 5;
+    double area = Geometry::Area(val);
 
     return 0;
 }
