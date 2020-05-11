d0622b4f9d708cdf7a662f8d057a8fa135815047
Sebastian Dixon <smedixon@gmail.com>
2020-05-11 13:15:14 +0100

    virtual function overriding and inheritance

--- main.cpp
+++ main.cpp
@@ -1,9 +1,72 @@
-#include "Handle.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+    protected:
+    string name;
+    int age;
+
+    public:
+    virtual void displayProp() const;
+
+    Person(){}
+
+    Person(string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+
+    ~Person(){}
+
+};
+
+void Person::displayProp() const
+{
+    cout << name << '\n' << age << endl;
+}
+
+class Student:public Person
+{
+    private:
+    string college;
+
+    public:
+    virtual void displayProp() const;
+
+    Student(){}
+
+    Student(string college, string name,  int age)
+    {
+        this->college = college;
+        this->name = name;
+        this->age = age;
+    }
+
+    ~Student(){}
+
+};
+
+void Student::displayProp() const
+{
+    cout <<'\n';
+    Person::displayProp();
+    cout << college << endl;
+}
+
 
 int main()
 {
-    Handle h;
-    h.bodyOperation();
+
+    Person p1{"Rick", 50};
+    p1.displayProp();
+
+    Student s1{"UCL", "Fred", 19};
+    s1.displayProp();
+    
 
     return 0;
 }
