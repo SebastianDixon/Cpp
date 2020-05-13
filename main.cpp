d39dd8c3bbf25566a7b12f85b64a157accc928f2
Sebastian Dixon <smedixon@gmail.com>
2020-05-13 16:03:33 +0100

    source file, comments

--- main.cpp
+++ main.cpp
@@ -1,25 +1,8 @@
 #include <iostream>
-#include "SavingsAccount.h"
-
-using namespace std;
-
-void FreezeMe(Freeze & f)
-{
-    f.freeze();
-}
 
 
 int main()
 {
-    SavingsAccount a1{1000, 1.025, false};
-
-    a1.deposit(999);
-    a1.withdraw(50);
-    a1.earnInterest();
-
-    FreezeMe(a1); //this implements the freezable class
-
-    a1.unfreeze();
 
     return 0;
 }
