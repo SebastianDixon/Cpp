e98b392dac0641ba465e8419da0a84a4509eb156
Sebastian Dixon <smedixon@gmail.com>
2020-05-18 14:51:24 +0100

    iostream operators, iostream class object overiding

--- main.cpp
+++ main.cpp
@@ -2,9 +2,32 @@
 #include <string>
 using namespace std;
 
+class point
+{
+    public:
+    int x,y;
+
+};
+
+
+std::istream & operator >> (std::istream & is, point & p)
+{
+    is >> p.x >> p.y;
+    return is;
+}
 
 int main()
 {
+    string s;
+    cout << "what is your string? : ";
+    cin >> s;
+    cout << s << " is your string" << endl;
+
+    point p1;
+    cout << "enter the x then y value for your object: ";
+    cin >> p1.x >> p1.y;
+    cout << p1.x << endl;
+    cout << p1.y << endl;
 
     return 0;
 }
