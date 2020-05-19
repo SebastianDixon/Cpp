b74ba6c03ff58708dd09acb5cf72a5bb1c16479c
Sebastian Dixon <smedixon@gmail.com>
2020-05-19 14:28:16 +0100

    justifications, manipulators, string streams

--- main.cpp
+++ main.cpp
@@ -1,39 +1,18 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
-class point
-{
-    public:
-    int x,y;
-
-};
-
-
-istream & operator >> (istream & is, point & p)
-{
-    is >> p.x >> p.y;
-    return is;
-}
-
-ostream & operator << (ostream & os, const point & p)
-{
-    os << "["<< p.x << p.y << "]";
-    return os;
-}
 
 int main()
 {
-    string s;
-    cout << "what is your string? : ";
-    cin >> s;
-    cout << s << " is your string" << endl;
+    
 
-    point p1;
-    cout << "enter the x then y value for your object: ";
-    cin >> p1.x >> p1.y;
-    cout << p1.x << endl;
-    cout << p1.y << endl;
+
+    cout << hex;
+    cout << 205 << endl;
+    cout << uppercase << 205 << endl; 
+    cout << nouppercase << 205 << endl;
 
     return 0;
 }
