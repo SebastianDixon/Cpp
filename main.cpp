fece83d0526e4f3fc606ab33713654ac23e1281d
Sebastian Dixon <smedixon@gmail.com>
2020-05-20 11:05:14 +0100

    file streams, read and write with ifstream and ofstream functions

--- main.cpp
+++ main.cpp
@@ -1,18 +1,49 @@
 #include <iostream>
 #include <string>
-#include <iomanip>
+#include <fstream>
 using namespace std;
 
+void fileWrite()
+{
+    ofstream ofile;
+
+    ofile.open("file1.txt");
+    if(ofile.is_open())
+    {
+        ofile << "Line 1" << endl;
+        ofile << "Line 2" << endl << "alksdjhfalkfhkfhl" << endl;
+        ofile << "Line 4" << endl;
+        ofile.close();
+    }
+    else
+    {
+        cout << "couldnt open file" << endl;
+    }
+}
+
+void fileRead()
+{
+    ifstream ifile;
+    ifile.open("file1.txt");
+    if(ifile.is_open())
+    {
+        string line;
+        while(getline(ifile, line))
+        {
+            cout << line << endl;
+        }
+        ifile.close();
+    }
+    else
+    {
+        cout << "couldnt read from file" << endl;
+    }
+}
 
 int main()
 {
-    
-
-
-    cout << hex;
-    cout << 205 << endl;
-    cout << uppercase << 205 << endl; 
-    cout << nouppercase << 205 << endl;
+    fileWrite();
+    fileRead();
 
     return 0;
 }
