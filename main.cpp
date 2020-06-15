f4d50409a8fe008e8ff5077bffe639094094530c
Sebastian Dixon <smedixon@gmail.com>
2020-06-15 15:10:49 +0100

    adv course branch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+using namespace std;
+
+class Thing{
+public:
+    string name;
+    int age;
+
+    Thing(){}
+
+    Thing(string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+
+    ~Thing()= default;
+
+    void details() {
+        cout << name << "" << age << endl;
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    Thing p1{"Seb", 18};
+
+    p1.details();
+
+
     return 0;
 }
