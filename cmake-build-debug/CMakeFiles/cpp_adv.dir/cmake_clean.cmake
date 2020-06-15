f072791d8f7807febf053f2f4ee66c6a293f6523
Sebastian Dixon <smedixon@gmail.com>
2020-06-15 14:56:03 +0100

    push

---
+++ cmake-build-debug/CMakeFiles/cpp_adv.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_adv.dir/main.cpp.obj"
+  "cpp_adv.exe"
+  "cpp_adv.exe.manifest"
+  "cpp_adv.pdb"
+  "libcpp_adv.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_adv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
